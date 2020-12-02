1bf23b76d6c72d60b86baaae1dda6ab74b2705eb
Sandip <dalvadisandip123@gmail.com>
2020-12-02 22:15:08 +0530

    add project

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    // public function handle($request, Closure $next)
+    // {
+    //     return $next($request);
+    // }
+    public function handle($request, Closure $next)
+    {
+        if(auth()->user()->role == 1){
+            return $next($request);
+        }
+   
+        return redirect('home')->with('error',"You don't have admin access.");
+    }
+}
